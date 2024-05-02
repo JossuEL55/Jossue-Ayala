ca5b7177ed12716fa807d57501fb69f11b847105
laboratorio <laboratorio@UPOAULA10609>
2024-05-02 07:31:49 -0500

    Agregar archivos de proyecto.

---
+++ Jossue Ayala/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Jossue_Ayala.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
